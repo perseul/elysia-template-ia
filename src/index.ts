e31d7ab4f6e67427b850be52fe3d754fce7ba261
perseul <perseulmeira@gmail.com>
2024-01-27 12:28:14 -0300

    update on swagger

--- src/index.ts
+++ src/index.ts
@@ -1,18 +1,18 @@
 import Elysia from "elysia";
 import { securitySetup } from "./startup/security";
 import './database/db.setup';
-import { docsSetup } from "./startup/docs";
 import { logger } from "@grotto/logysia";
 import { hooksSetup } from "./startup/hooks";
 import { usuariosController } from "./controllers/usuarios.controller";
 import { staticDataController } from "./controllers/static-data.controller";
+import swagger from "@elysiajs/swagger";
 
 const PORT = process.env.PORT || 3000;
 export const app = new Elysia();
 
 app
-  //.use(securitySetup)
-  .use(docsSetup)
+  .use(securitySetup)
+  .use(swagger())
   .use(logger())
   .use(hooksSetup)
   .get('/', () => 'Olá Bun.js!')
